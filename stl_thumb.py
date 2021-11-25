bbb32dfb8b9646ffb1750859a9b3c7e52bbde7e9
David Flemström <david.flemstrom@gmail.com>
2021-11-25 16:10:54 +0100

    Convert to python3 and make code less susceptible to command injection

--- stl_thumb.py
+++ stl_thumb.py
@@ -1,44 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-import os
+import subprocess
 import sys
-import time
-import hashlib
+import tempfile
 
-fin = ""
-fout = ""
-size = ""
 
 def main():
+    if len(sys.argv) != 4:
+        print(f"{sys.argv[0]}: usage: {sys.argv[0]} <input path> <output path> <size>", file=sys.stderr)
+        return 1
 
-    m = hashlib.md5()
-    m.update(fin)
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
+    size = sys.argv[3]
 
-    ff = "/tmp/stl_to_png_%s.scad" % m.hexdigest()
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(f'import("{input_path}");')
+        f.flush()
+        subprocess.call(["openscad", "-o", output_path, f"--imgsize={size},{size}", f.name])
 
-    f = open(ff, "w")
-    f.write("import(\"%s\");" % fin)
-    f.close()
+    return 0
 
-    cmd = "openscad -o %s.png --imgsize=%s,%s %s 2> /dev/null; mv %s.png %s" % (fout, size, size, ff, fout, fout)
-    os.system(cmd)
-
-    os.remove(ff)
-
-    sys.exit(0)
 
 if __name__ == '__main__':
-
-    f = open("/tmp/stl_to_png_tmp.log", "w")
-    f.write("%i %s" % (len(sys.argv), sys.argv))
-    f.close()
-
-    if len(sys.argv) != 4:
-        print "add args [in file] [out file] [size]"
-        sys.exit(0)
-    else:
-        fin = sys.argv[1]
-        fout = sys.argv[2]
-        size = sys.argv[3]
-
-    main()
+    sys.exit(main())
